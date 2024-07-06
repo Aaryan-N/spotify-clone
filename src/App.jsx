148f0b7eb7cf292b04825fe3ef945fcdc6df12e6
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-07-06 18:59:39 +1000

    Initial commit with tailwind and react configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Sidebar from "./components/Sidebar.jsx"
+import Player from "./components/Player.jsx";
 
 const App = () => {
     return (
-        <div>
-            
+        <div className="h-screen bg-black">
+            <div className="h-[90%] flex">
+                <Sidebar />
+            </div>
+            <Player />
         </div>
     );
 };
